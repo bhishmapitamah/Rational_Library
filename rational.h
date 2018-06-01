11c6cc65527680e7ce1d10074baf1af8d6d4a439
bhishmapitamah <pandeyashutosh1986@yahoo.com>
2018-06-01 16:26:09 +0530

    Initial commit

---
+++ rational.h
@@ -0,0 +1,58 @@
+#ifndef _cpp_rational
+#define _cpp_rational
+#include <iostream>
+
+typedef long long Type;
+
+class rational{
+    Type _num;
+    Type _den;
+    bool sign;
+public:
+    rational();
+    explicit rational(Type,Type=1);
+
+    Type GCD(Type,Type);
+    void simplify();
+
+    rational& operator+=(const rational&);
+    rational& operator-=(const rational&);
+    rational& operator*=(const rational&);
+    rational& operator/=(const rational&);
+
+    rational operator-();
+    rational& operator+();
+
+    friend std::ostream& operator<<(std::ostream&,const rational&);
+    friend std::istream& operator>>(std::istream&,rational&);
+
+    explicit operator const int() const {return (int)(_num/_den);}
+    explicit operator const long() const {return (long)(_num/_den);}
+    explicit operator const long long() const {return ((long long)_num/_den);}
+    explicit operator const float() const {return ((float)_num/_den);}
+    explicit operator const double() const {return ((double)_num/_den);}
+
+    const Type den()const{return _den;}
+    const Type num()const{return _num;}
+
+    Type den(Type);
+    Type num(Type);
+};
+
+inline rational operator+(rational, const rational&);
+inline rational operator-(rational, const rational&);
+inline rational operator*(rational, const rational&);
+inline rational operator/(rational, const rational&);
+
+inline bool operator<(const rational&, const rational&);
+inline bool operator>(const rational&, const rational&);
+inline bool operator<=(const rational&, const rational&);
+inline bool operator>=(const rational&, const rational&);
+inline bool operator==(const rational&, const rational&);
+inline bool operator!=(const rational&, const rational&);
+
+inline rational abs(const rational&);
+rational pow(rational);
+
+
+#endif
